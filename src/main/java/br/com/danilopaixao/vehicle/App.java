3ec694e6cd770c90e2ae39e15b912ec74f8a2bbd
Danilo GingaOne <danilo.paixao@gingaone.com>
2019-07-17 19:55:32 -0300

    solve bugfix client eureka

--- src/main/java/br/com/danilopaixao/vehicle/App.java
+++ src/main/java/br/com/danilopaixao/vehicle/App.java
@@ -1,26 +1,42 @@
 package br.com.danilopaixao.vehicle;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-@RestController
-public class App{
+public class App { //EurekaClientApplication
 
-    @Value("${spring.application.name}")
-    private String appName;
- 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
- 
-    @RequestMapping("/springboot")
-    public String greeting() {
-    	return "it works: "+ appName;
+}
+
+@RestController
+class ServiceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
     }
-	
+
+    @RequestMapping("/actuator/info")
+    //public List<ServiceInstance> actuatorInfo() {
+    public String actuatorInfo() {
+        //return this.discoveryClient.getInstances("vehicle");
+        return "it works";
+    }
 }
