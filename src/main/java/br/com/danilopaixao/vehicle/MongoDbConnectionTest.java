8032a83e7be1d1a568cacc9fbea030bf6fba097d
Danilo GingaOne <danilo.paixao@gingaone.com>
2019-11-04 22:36:53 -0300

    refactory dev env works

--- src/main/java/br/com/danilopaixao/vehicle/MongoDbConnectionTest.java
+++ src/main/java/br/com/danilopaixao/vehicle/MongoDbConnectionTest.java
@@ -10,8 +10,12 @@ import com.mongodb.client.MongoDatabase;
 public class MongoDbConnectionTest {
 	
 	public static void main(String[] args) {
+		//https://mlab.com/databases/springclouddb  ==> danilopaixao - da88878685
+//		MongoClientURI uri = new MongoClientURI(
+//			    "mongodb+srv://admin:senha123@cluster0-1h4ej.mongodb.net/test?retryWrites=true&w=majority");
+		//mongodb://<dbuser>:<dbpassword>@ds141208.mlab.com:41208/springclouddb
 		MongoClientURI uri = new MongoClientURI(
-			    "mongodb+srv://admin:admin@cluster0-1h4ej.mongodb.net/test?retryWrites=true&w=majority");
+			    "mongodb://admin:senha123@ds141208.mlab.com:41208/springclouddb");
 
 			MongoClient mongoClient = new MongoClient(uri);
 			MongoDatabase database = mongoClient.getDatabase("springclouddb");
