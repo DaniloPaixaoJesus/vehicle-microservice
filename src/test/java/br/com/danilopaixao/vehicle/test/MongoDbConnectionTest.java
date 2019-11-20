0f9d9aa6d76c932cb41c7da31e7712a10ace2429
DaniloPaixaoJesus <contato.danilo.paixao@gmail.com>
2019-11-20 14:42:08 -0300

    setup project in order to run on docker, local and dev environment

--- src/test/java/br/com/danilopaixao/vehicle/test/MongoDbConnectionTest.java
+++ src/test/java/br/com/danilopaixao/vehicle/test/MongoDbConnectionTest.java
@@ -11,9 +11,10 @@ public class MongoDbConnectionTest {
 	
 	public static void main(String[] args) {
 		//https://mlab.com/databases/springclouddb  ==> danilopaixao - da88878685
+		
 //		MongoClientURI uri = new MongoClientURI(
-//			    "mongodb+srv://admin:senha123@cluster0-1h4ej.mongodb.net/test?retryWrites=true&w=majority");
-		//mongodb://<dbuser>:<dbpassword>@ds141208.mlab.com:41208/springclouddb
+//			    "mongodb://localhost:27017/springclouddb");
+		
 		MongoClientURI uri = new MongoClientURI(
 			    "mongodb://admin:senha123@ds141208.mlab.com:41208/springclouddb");
 
