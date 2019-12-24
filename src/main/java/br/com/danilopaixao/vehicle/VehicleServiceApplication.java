04351800592ff4ad963138b11e9012971e272c39
Danilo Paixao de Jesus - RCR <djesus.rcr@veloe.com.br>
2019-12-24 11:10:50 -0300

    vehicle rest api remove cross verification

--- src/main/java/br/com/danilopaixao/vehicle/VehicleServiceApplication.java
+++ src/main/java/br/com/danilopaixao/vehicle/VehicleServiceApplication.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,6 +20,24 @@ public class VehicleServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VehicleServiceApplication.class, args);
+    }
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
     
     @Bean
